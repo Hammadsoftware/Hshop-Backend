3848a671d2d2433e35fd1518fbdbeb145595868c
Hammad Tariq <165913611+Hammadsoftware@users.noreply.github.com>
2025-06-28 19:00:43 +0500

    Update App.js
    d

--- App.js
+++ App.js
@@ -1,37 +1,34 @@
-// importing the required modules
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 import connectDB from './config/mongoConnect.js';
-import dotenv from 'dotenv';
 import router from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
-import cookieParser from "cookie-parser";
 import cartRouter from './routes/cartRoute.js';
-
-dotenv.config();
-connectDB(); // connecting to the database
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+connectDB();
+
 app.use(cors({
-    origin: 'https://h-shop-production.up.railway.app', // make sure this matches frontend origin
+    origin: 'https://h-shop-production.up.railway.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
 
-app.get('/',(req,res)=>{
-return Hello world;
-}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// importing the routes
 app.use('/user', router);
 app.use('/product', productRouter);
 app.use('/cart', cartRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
